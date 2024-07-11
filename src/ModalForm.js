cd49a307131668af4bf08734c3242718c33a354d
premnewbie <prempm50@gmail.com>
2024-07-11 10:14:41 +0530

    order change

--- src/ModalForm.js
+++ src/ModalForm.js
@@ -31,12 +31,10 @@ export default function ModalForm() {
     const emailFormat = /\S+@\S+\.\S+/;
     const phoneFormat = /^\d{10}$/;
 
-    if (dob!=='' && new Date(dob) > currentDate) {
-      alert("Invalid date of birth. Please enter a valid date.");
-    } else if (!emailFormat.test(email)) {
-      alert("Invalid email. Please check your email address.");
-    } else if (!phoneFormat.test(phone)) {
+    if (!phoneFormat.test(phone)) {
       alert("Invalid phone number. Please enter a 10-digit phone number.");
+    } else if (dob!=='' && new Date(dob) > currentDate) {
+      alert("Invalid date of birth. Please enter a valid date.");
     } else{
       setUsername('');
       setEmail('');
